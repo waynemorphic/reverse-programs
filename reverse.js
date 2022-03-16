b2f0150fda4b2ef36a86c22440dab64ac32cc082
waynemorphic <wayneklyde@gmail.com>
2022-03-16 23:40:23 +0300

    Method and function to reverse numbers stored in an array

---
+++ reverse.js
@@ -0,0 +1,25 @@
+// A method to reverse numbers stored in an array
+
+let numbers = [1, 2, 3, 4, 5]
+
+let numericals = [];
+
+for (i = numbers.length - 1; i >= 0; i--){
+    numericals = numericals + numbers[i];
+    numbers.push(numericals);
+}
+
+console.log (numericals);
+
+// A function to reverse numbers stored in an array
+
+function numbersReverse(reverse){
+    let numericals = [];
+
+    for (i = reverse.length - 1; i >= 0; i --){
+        numericals = numericals + reverse[i];
+    }
+    return numericals;
+}
+
+console.log(numbersReverse([1,2,3,4,5,6]));
